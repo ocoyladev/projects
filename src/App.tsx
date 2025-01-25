72cd3867449f9c42bdf18eea5c34af6d4e76e9ea
ocoyladev <ocoyladev@gmail.com>
2025-01-24 23:19:13 -0500

    Adding folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Contact from './pages/contact';
+import Main from './pages/main';
+import PersonalInfo from './pages/personal-info';
+import Projects from './pages/projects';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
+  return (    
+    <Router>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,12 +26,18 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Let's make magic
-        </p>
+        <p>Let's make magic</p>
       </div>
-      <p> Here should be my projects, just wait</p>
-    </>
+      <p>Here should be my projects, just wait</p>
+      <Routes>
+        <Route path="/" element={<Main/>} />
+        <Route path="/personal-info" element={<PersonalInfo/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+
+
   )
 }
 
---
+++ src/pages/personal-info.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+    return (
+        <div>personal-info</div>
+    )
+}
+
+export default PersonalInfo
